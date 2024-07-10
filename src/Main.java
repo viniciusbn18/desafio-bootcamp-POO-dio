7953812f600d9d140b0b0786c61e2a20952238a7
vinicius <vinicius.neves@al.infnet.edu.br>
2024-07-10 11:00:55 -0300

    firt commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao curso java");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+    }
+}
---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,42 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate dataDaMentoria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataDaMentoria() {
+        return dataDaMentoria;
+    }
+
+    public void setDataDaMentoria(LocalDate dataDaMentoria) {
+        this.dataDaMentoria = dataDaMentoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataDaMentoria=" + dataDaMentoria +
+                '}';
+    }
+}
