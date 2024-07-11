d120e1826a36896c340d1601f574d49cc88ddc2d
vinicius <vinicius.neves@al.infnet.edu.br>
2024-07-11 16:52:09 -0300

    commit

--- src/br/com/dio/desafio/dominio/Bootcamp.java
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -1,4 +1,80 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class Bootcamp {
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+    private Set<Dev> listadeAlunos = new HashSet<>();
+    private Set<Conteudo> listaDeConteudos = new LinkedHashSet<>();
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public Set<Dev> getListadeAlunos() {
+        return listadeAlunos;
+    }
+
+    public void setListadeAlunos(Set<Dev> listadeAlunos) {
+        this.listadeAlunos = listadeAlunos;
+    }
+
+    public Set<Conteudo> getListaDeConteudos() {
+        return listaDeConteudos;
+    }
+
+    public void setListaDeConteudos(Set<Conteudo> listaDeConteudos) {
+        this.listaDeConteudos = listaDeConteudos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Bootcamp bootcamp)) return false;
+        return Objects.equals(getNome(),
+                bootcamp.getNome()) && Objects.equals(getDescricao(),
+                bootcamp.getDescricao()) && Objects.equals(getDataInicial(),
+                bootcamp.getDataInicial()) && Objects.equals(getDataFinal(),
+                bootcamp.getDataFinal()) && Objects.equals(getListadeAlunos(),
+                bootcamp.getListadeAlunos()) && Objects.equals(getListaDeConteudos(),
+                bootcamp.getListaDeConteudos());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(),
+                getDescricao(),
+                getDataInicial(),
+                getDataFinal(),
+                getListadeAlunos(),
+                getListaDeConteudos());
+    }
 }
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,4 +1,77 @@
 package br.com.dio.desafio.dominio;
 
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
 public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getListaDeConteudos());
+        bootcamp.getListadeAlunos().add(this);
+    }
+
+    public void progrecao(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum BootCamp");
+        }
+    }
+
+    public double calcularTotalXP(){
+      return this.conteudosConcluidos
+              .stream()
+              .mapToDouble(conteudo -> conteudo.calcularXP())
+              .sum();
+    }
+
+    public void exibirCursos(){
+        System.out.println(conteudosInscritos);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudoConcluido() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudoConcluido(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    public Set<Conteudo> getContetudoInscrito() {
+        return conteudosInscritos;
+    }
+
+    public void setContetudoInscrito(Set<Conteudo> contetudoInscrito) {
+        this.conteudosInscritos = contetudoInscrito;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Dev dev)) return false;
+        return Objects.equals(getNome(),
+                dev.getNome()) && Objects.equals(getConteudoConcluido(),
+                dev.getConteudoConcluido()) && Objects.equals(getContetudoInscrito(),
+                dev.getContetudoInscrito());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getConteudoConcluido(), getContetudoInscrito());
+    }
 }
