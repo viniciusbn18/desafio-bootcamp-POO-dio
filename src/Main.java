00bfdcc56373865fa17aaa7140f5c94dd36300d8
vinicius <vinicius.neves@al.infnet.edu.br>
2024-07-11 17:28:29 -0300

    testando o código

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,48 @@
-import br.com.dio.desafio.dominio.Conteudo;
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso de POO com Java");
+        curso1.setDescricao("Descrição curso 1");
+        curso1.setCargaHoraria(20);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curos C++");
+        curso2.setDescricao("Descrição curso 2");
+        curso2.setCargaHoraria(30);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria para desenvolvimento Ágil");
+        mentoria.setDescricao("Descrição da mentoria");
+
+
+
+        Bootcamp backEndJava = new Bootcamp();
+        backEndJava.setNome("Back-end Java");
+        backEndJava.setDescricao("Descricao do bootcamp");
+        backEndJava.getListaDeConteudos().add(curso1);
+        backEndJava.getListaDeConteudos().add(curso2);
+        backEndJava.getListaDeConteudos().add(mentoria);
+
+
+        Dev vinicius = new Dev();
+
+        vinicius.setNome("Vinicius");
+        vinicius.inscreverBootcamp(backEndJava);
+        System.out.println("Conteudos inscritos: " + vinicius.getContetudoInscrito());
+        vinicius.progrecao();
+        System.out.println("Conteudos inscritos: " + vinicius.getContetudoInscrito());
+
+
+
+
+
 
     }
 }
